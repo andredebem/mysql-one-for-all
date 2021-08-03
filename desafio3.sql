435663a39cc2c1fbf4edc66149c3a140c4ae923d
André De Bem <andrebemferraz@gmail.com>
2021-08-03 19:05:02 -0300

    Request III - Code Climate Fix I

--- desafio3.sql
+++ desafio3.sql
@@ -1,13 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.username AS 'usuario',
-       s.song_name AS 'nome'
+        s.song_name AS 'nome'
 FROM SpotifyClone.users u
 INNER JOIN SpotifyClone.songs s
 WHERE EXISTS
     (SELECT *
-     FROM SpotifyClone.history_of_plays h
-     WHERE h.user_id = u.user_id
-       AND h.song_id = s.song_id)
+      FROM SpotifyClone.history_of_plays h
+      WHERE h.user_id = u.user_id
+        AND h.song_id = s.song_id)
 ORDER BY usuario,
-         nome;
-    
+          nome;
