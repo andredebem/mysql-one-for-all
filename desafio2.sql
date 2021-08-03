aaf8f488ca55badeba4e5b8c59fddea21f1a7ab4
André De Bem <andrebemferraz@gmail.com>
2021-08-03 18:53:26 -0300

    Request II - Code Climate Fix I

--- desafio2.sql
+++ desafio2.sql
@@ -2,8 +2,8 @@ CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(*) AS 'cancoes',
 
   (SELECT COUNT(*)
-   FROM SpotifyClone.artists) AS 'artistas',
+    FROM SpotifyClone.artists) AS 'artistas',
 
   (SELECT COUNT(*)
-   FROM SpotifyClone.albuns) AS 'albuns'
+    FROM SpotifyClone.albuns) AS 'albuns'
 FROM SpotifyClone.songs;
