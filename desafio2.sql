7489ed9dcb6fb3f598275a45f79a5d1ebe4fb52b
André De Bem <andrebemferraz@gmail.com>
2021-08-03 18:52:29 -0300

    Request II

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(*) AS 'cancoes',
+
+  (SELECT COUNT(*)
+   FROM SpotifyClone.artists) AS 'artistas',
+
+  (SELECT COUNT(*)
+   FROM SpotifyClone.albuns) AS 'albuns'
+FROM SpotifyClone.songs;
