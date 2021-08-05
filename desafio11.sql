0b3c1bba58e711723045dd5aec7573251bef2c32
André De Bem <andrebemferraz@gmail.com>
2021-08-05 17:51:49 -0300

    Request XI

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+CREATE VIEW cancoes_premium AS
+SELECT s.song_name AS 'nome',
+       COUNT(h.song_id) AS 'reproducoes'
+FROM SpotifyClone.songs s
+INNER JOIN SpotifyClone.history_of_plays h ON s.song_id = h.song_id
+WHERE EXISTS
+    (SELECT *
+     FROM SpotifyClone.users u
+     INNER JOIN SpotifyClone.plans p
+     WHERE h.user_id = u.user_id
+       AND u.selected_plan = p.plan_id
+       AND p.plan_name IN ('familiar', 'universitário'))
+GROUP BY nome
+ORDER BY nome;
