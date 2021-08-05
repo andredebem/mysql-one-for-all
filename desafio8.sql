39b86ad03387d9b6a23b1ae2264842b6f4174634
André De Bem <andrebemferraz@gmail.com>
2021-08-05 17:06:41 -0300

    Request VIII - Small Update II

--- desafio8.sql
+++ desafio8.sql
@@ -5,9 +5,9 @@ AFTER DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 
-DELETE FROM SpotifyClone.follows_list WHERE OLD.user_id = SpotifyClone.follows_list.user_id;
-DELETE FROM SpotifyClone.history_of_plays WHERE OLD.user_id = SpotifyClone.history_of_plays.user_id;
-
+DELETE FROM SpotifyClone.follows_list WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.history_of_plays WHERE user_id = OLD.user_id;
+-- Precisei dar uma estudada pelos códigos dos colegas para compreender onde estava o erro
 END $$
 
 DELIMITER ;
