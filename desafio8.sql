b9d4228f74ac136dcec6b0a981b31c92d91cc132
André De Bem <andrebemferraz@gmail.com>
2021-08-05 17:18:29 -0300

    Request VIII - Small Update III

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,15 @@
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.users
+BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 
 DELETE FROM SpotifyClone.follows_list WHERE user_id = OLD.user_id;
 DELETE FROM SpotifyClone.history_of_plays WHERE user_id = OLD.user_id;
+
 -- Precisei dar uma estudada pelos códigos dos colegas para compreender onde estava o erro
+
 END $$
 
 DELIMITER ;
